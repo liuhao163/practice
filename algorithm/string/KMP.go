19998da427e3bc7b3b5f40733dbb7e4cc71daca3
liuhaoeric <liuhaoeric@didichuxing.com>
2019-07-21 10:24:51 +0800

    main

--- algorithm/string/KMP.go
+++ algorithm/string/KMP.go
@@ -1,12 +1,13 @@
 package main
 
+
 func KMP(s string, p string) int {
 	nexts := getNext(p)
 
 	j := 0
 	for i := 0; i < len(s); i++ {
 		for ; j > 0 && s[i] != p[j]; {
-			j = nexts[j-1] + 1
+			j = nexts[j-1]
 		}
 
 		if s[i] == p[j] {
@@ -14,26 +15,28 @@ func KMP(s string, p string) int {
 		}
 
 		if j == len(p) {
+			//因为s[i] == s[j] 一直到pattern结束所以，下标应该是i-(len(p)-1)
 			return i - (len(p) - 1)
 		}
-	}
 
+	}
 	return -1
 }
 
 func getNext(p string) []int {
 	ret := make([]int, len(p))
-	ret[0] = -1
-	k := -1
+	ret[0]=0
+	maxLen := 0
 	for i := 1; i < len(p); i++ {
-		for ; k != -1 && p[i] != p[k+1]; {
-			k = ret[k]
+		for ; maxLen > 0 && p[maxLen] != p[i]; {
+			maxLen = ret[maxLen-1]
 		}
 
-		if p[k+1] == p[i] {
-			k++
+		if p[maxLen] == p[i] {
+			maxLen++
 		}
-		ret[i] = k
+
+		ret[i] = maxLen
 	}
 
 	return ret;
